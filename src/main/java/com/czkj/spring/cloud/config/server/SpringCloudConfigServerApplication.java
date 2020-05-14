c242d1d0104bf60972a8ce3ebde1010f8299f57c
shengyinyuan <zhongba520@126.com>
2020-05-14 17:17:12 +0800

    init

---
+++ src/main/java/com/czkj/spring/cloud/config/server/SpringCloudConfigServerApplication.java
@@ -0,0 +1,20 @@
+package com.czkj.spring.cloud.config.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableConfigServer
+public class SpringCloudConfigServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCloudConfigServerApplication.class, args);
+		System.out.println("-------------------------------------------------------------------");
+		System.out.println("spring cloud config server 配置中心启动");
+		System.out.println("-------------------------------------------------------------------");
+	}
+
+}

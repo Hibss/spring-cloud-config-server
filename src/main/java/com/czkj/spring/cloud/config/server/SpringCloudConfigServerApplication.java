ccf3208460b1cc1920819846ade2fea733d883dd
shengyinyuan <zhongba520@126.com>
2020-05-14 19:21:59 +0800

    1

--- src/main/java/com/czkj/spring/cloud/config/server/SpringCloudConfigServerApplication.java
+++ src/main/java/com/czkj/spring/cloud/config/server/SpringCloudConfigServerApplication.java
@@ -2,10 +2,12 @@ package com.czkj.spring.cloud.config.server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
-@SpringBootApplication
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class,HibernateJpaAutoConfiguration.class})
 @EnableDiscoveryClient
 @EnableConfigServer
 public class SpringCloudConfigServerApplication {
